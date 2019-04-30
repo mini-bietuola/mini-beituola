fdb0e1c1c6695d880d9cfdb8c7b664257b70f554
zhangdifang <zhangdifang>
2019-04-30 16:36:49 +0800

    User类的新增、根据userId修改、根据userId查询User

--- src/main/java/com/netease/mini/bietuola/mapper/UserMapper.java
+++ src/main/java/com/netease/mini/bietuola/mapper/UserMapper.java
@@ -1,9 +1,29 @@
 package com.netease.mini.bietuola.mapper;
 
+import com.netease.mini.bietuola.entity.User;
+
 /**
  * @Description
  * @Auther ctl
  * @Date 2019/4/30
  */
 public interface UserMapper {
+
+    /**
+     * 根据用户id查找用户
+     * @param userId 用户id
+     * @return
+     */
+    User findUserByUserId(int userId);
+
+
+    /**
+     * @param user 用户
+     */
+    void save(User user);
+
+    /**
+     * @param user 用户
+     */
+    void updateByUserId(User user);
 }
