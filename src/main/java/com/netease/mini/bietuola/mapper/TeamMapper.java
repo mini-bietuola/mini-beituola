616db8c0c77f8d460c01caff53983368061c3e5e
zhangdifang <zhangdifang>
2019-04-30 17:56:55 +0800

    UserTeam类新增根据用户Id查询对应的UserTeam对象

--- src/main/java/com/netease/mini/bietuola/mapper/TeamMapper.java
+++ src/main/java/com/netease/mini/bietuola/mapper/TeamMapper.java
@@ -1,9 +1,18 @@
 package com.netease.mini.bietuola.mapper;
 
+import com.netease.mini.bietuola.constant.TeamStatus;
+import com.netease.mini.bietuola.entity.Team;
+
 /**
  * @Description
  * @Auther ctl
  * @Date 2019/4/30
  */
 public interface TeamMapper {
+    /**
+     * @param id
+     * @param teamStatus
+     * @return
+     */
+    Team findTeamByTeamIdAndActivityStatus(Long id, TeamStatus teamStatus);
 }
